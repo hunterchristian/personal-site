ea5ec1a617bb6e8d2a15902bfcdf518700a0af9f
Hunter Hodnett <hhodnett@amazon.com>
2016-11-24 16:37:03 -0800

    Adding the equalizer as a component.

--- src/js/main.js
+++ src/js/main.js
@@ -2,8 +2,39 @@
 
 import 'babel-polyfill';
 import Ractive from 'ractive';
+import Equalizer from './components/Equalizer';
+
+Ractive.components.equalizer = Equalizer;
 
 const ractive = new Ractive({
-    el: '.ractiveEl',
-    template: '<div>my new ractive!!!!</div>'
+    el: '.sect2',
+    template: '#sect2_template',
+    data: function () {
+        equalizerActive: false
+    },
+
+    /**
+     * When this section scrolls into view, activate the equalizer.
+     */
+    oninit: function () {
+        window.addEventListener('scroll', this.onscroll.bind(this));
+    },
+
+    /**
+     * We don't need to listen to the scroll event after this element is torn down.
+     */
+    onteardown: function () {
+        window.removeEventListener('scroll', this.onscroll.bind(this));
+    },
+
+    /**
+     * See if this section has scrolled into view and activate the equalizer if so.
+     */
+    onscroll: function () {
+        if (this.el.getBoundingClientRect().top <= this.el.clientHeight * 0.06) {
+            this.set('equalizerActive', true);
+        } else {
+            this.set('equalizerActive', false);
+        }
+    }
 });
